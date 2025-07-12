3fc1fcc0e4544fface8d7f106559285b66306ff1
Eymen Ünay <eymenunay@outlook.com>
2025-07-12 11:51:43 +0100

    Remove python linear-transform-lowering in favour of MLIR version

--- src/orion_heir/passes/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-ORION HEIR Passes
-
-This package contains various MLIR transformation passes for HEIR dialects.
-"""
-
-from .ckks_linear_transform_lowering import CKKSLinearTransformLoweringPass
-
-__all__ = ['CKKSLinearTransformLoweringPass']
