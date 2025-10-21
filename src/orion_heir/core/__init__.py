cc409939da9481cd5e06389ce1f832830293fa44
Jeremy Kun <j2kun@users.noreply.github.com>
2025-10-21 10:30:04 -0700

    drop constants

--- src/orion_heir/core/__init__.py
+++ src/orion_heir/core/__init__.py
@@ -4,15 +4,13 @@ from .translator import GenericTranslator, create_translator
 from .types import FHEOperation, SchemeParameters, FrontendInterface
 from .operation_registry import OperationRegistry
 from .type_builder import TypeBuilder
-from .constants import ConstantManager
 
 __all__ = [
-    'GenericTranslator',
-    'FHEOperation',
-    'SchemeParameters', 
-    'FrontendInterface',
-    'create_translator',
-    'OperationRegistry',
-    'TypeBuilder',
-    'ConstantManager',
+    "GenericTranslator",
+    "FHEOperation",
+    "SchemeParameters",
+    "FrontendInterface",
+    "create_translator",
+    "OperationRegistry",
+    "TypeBuilder",
 ]
